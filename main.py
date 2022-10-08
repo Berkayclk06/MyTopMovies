8722ed6df1d5a0649d87c4357ea0cd1efc7c8177
Berkay Colak <berkaycolak42@gmail.com>
2022-10-08 02:54:58 -0400

    update button activated and update-page implemented

--- main.py
+++ main.py
@@ -26,11 +26,44 @@ with app.app_context():
         img_url = db.Column(db.String(250), nullable=False)
 
     db.create_all()
+    # new_movie = Movies(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an"
+    #                 "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation "
+    #                 "with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    # db.session.add(new_movie)
+    # db.session.commit()
+
+
+class RateMovieForm(FlaskForm):
+    update_rating = StringField("Your Rating Out of 10, e.g. 7.5")
+    update_review = StringField("Your Review")
+    submit = SubmitField('Done')
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = db.session.query(Movies).all()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/update/<id>", methods=["GET", "POST"])
+def update(id):
+    form = RateMovieForm()
+    if form.validate_on_submit():
+        print(True)
+        movie_to_update = Movies.query.filter_by(id=id).first()
+        movie_to_update.review = form.update_review.data
+        movie_to_update.rating = form.update_rating.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", form=form, id=id)
 
 
 if __name__ == '__main__':
